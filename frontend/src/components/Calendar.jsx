2a872399bc7b2b361f13957d628363c1ddaa6e28
Karim Chehab <karimchehab2018@gmail.com>
2025-03-17 06:24:08 +0200

    - Fixed a small bug where "12PM-2PM" didn't show any events

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -5,7 +5,7 @@ function Calendar() {
     const timeslots = [
         "8AM-10AM",
         "10AM-12PM",
-        "12PM-2 PM",
+        "12PM-2PM",
         "2PM-4PM",
         "4PM-6PM",
         "6PM-8PM",
