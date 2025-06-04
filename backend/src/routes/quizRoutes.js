eeeab7e0fa94f323537b48b43c3980989ef288af
KarimChehab2003 <karimchehab2018@gmail.com>
2025-06-04 20:05:06 +0300

    - Added .env again for everyone to use

--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -3,7 +3,7 @@ import multer from "multer";
 import { uploadLecture } from "../controllers/quizController.js";
 
 const router = express.Router();
-const upload = multer({ dest: '../uploads/' });
+const upload = multer({ dest: 'uploads/' });
 
 
 router.post('/upload', upload.single('lecture'), uploadLecture)
