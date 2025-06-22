1079e82b4f2502b1be64f6eed0e8fd2fcdc107be
علي يحيي زكريا <alyyehiazak@gmail.com>
2025-06-22 22:45:33 +0300

    - Created a Demo Main Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import SignIn from "./pages/SignIn";
 import Registration from "./pages/Registration";
+import Main from "./pages/Main";
+import Layout from "./pages/Layout";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Calendar from "./components/Calendar";
-import Tasks from "./pages/Tasks";
-import Insights from "./pages/Insights";
 import { TasksProvider } from "./contexts/TasksContext";
 
 const App = () => {
@@ -18,7 +16,8 @@ const App = () => {
     <TasksProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Registration />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<Registration />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/dashboard/*" element={<Layout />} />
           <Route path="*" element={<Navigate to="/" replace />} />
